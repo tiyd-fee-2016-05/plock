a950fce3ab437d4d0f010610479eb30cc634a0de
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-10 01:14:07 -0400

    Add functionality so that ajax can get data from api and populate all fields so far on the page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,11 +11,18 @@ $( function () {
     //    console.log(value[0]);
     //  });
     // Array.from(data);
-    console.log(Array.from(data)[0].bookmark_name);
-    $(".place-here").text( Array.from(data)[0].bookmark_name );
+    // console.log(Array.from(data)[0].bookmark_name);
+    var bookmarkName = $(".savedBookmarkItem");
+    var bookmarkDescrip = $(".savedMarkDescrip");
+    var bookmarkURL = $(".savedMarkURL");
 
-
-
+    for( var index = 0; index < 2; index++ ) {
+      // console.log( bookmarks[index]);
+      // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
+      bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
+      bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
+      bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
+    }
 
    });
 });
