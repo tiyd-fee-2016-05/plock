de3cdcdd65292c85f926962a469d5001c37a9a86
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-10 11:25:00 -0400

    Add functionality so that ajax can get data from api and populate all fields so far on the page when user and password match

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 $( function () {
   "use strict";
 
+  // ajax get call for receiving bookmarks
+/*
   $.ajax({
     dataType: "JSON",
     url: "https://tiyeventapi.herokuapp.com/dummy"
@@ -25,4 +27,55 @@ $( function () {
     }
 
    });
+ */
+
+
+
+
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://8cc094dc.ngrok.io/my_bookmarks',
+    data: { "username":"fake", "password":"password" },
+  }).success(function (data) {
+    console.log("success");
+   $.ajax({
+     dataType: "JSON",
+     url: "https://tiyeventapi.herokuapp.com/dummy"
+    })
+    .success(function (data) {
+      console.log("success");
+     //  $.each( data.toArray(), function(index, value) {
+     //    console.log(value[0]);
+     //  });
+     // Array.from(data);
+     // console.log(Array.from(data)[0].bookmark_name);
+     var bookmarkName = $(".savedBookmarkItem");
+     var bookmarkDescrip = $(".savedMarkDescrip");
+     var bookmarkURL = $(".savedMarkURL");
+
+     for( var index = 0; index < 2; index++ ) {
+       // console.log( bookmarks[index]);
+       // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
+       bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
+       bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
+       bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
+     }
+
+    });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
