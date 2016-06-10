7bed27efaf50e9c35e7a585800313c30214ca6cd
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-10 16:48:35 -0400

    Add functionality so that when user submits a new bookmark, the fields are cleared

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,67 +1,38 @@
 $( function () {
   "use strict";
 
-  // ajax get call for receiving bookmarks
-/*
+  // submit event to login user
+  $('.login').on('submit', function (e) {
+    e.preventDefault();
+
+    var plockUser = $('.userInfo[name="user"]').val();
+    var plockPassword = $('.passwordInfo[name="password"]').val();
+    console.log(plockUser);
+    console.log(plockPassword);
+
+  // GET will submit username and password and then retrieve their bookmarks
   $.ajax({
-    dataType: "JSON",
-    url: "https://tiyeventapi.herokuapp.com/dummy"
-   })
-   .success(function (data) {
-     console.log("success");
-    //  $.each( data.toArray(), function(index, value) {
-    //    console.log(value[0]);
-    //  });
-    // Array.from(data);
-    // console.log(Array.from(data)[0].bookmark_name);
+    method: 'GET',
+    url: 'https://dummyplock.herokuapp.com/my_bookmarks',
+    data: { "username": plockUser, "password": plockPassword},
+  })
+    .success(function (data) {
+    console.log("success");
+
     var bookmarkName = $(".savedBookmarkItem");
     var bookmarkDescrip = $(".savedMarkDescrip");
     var bookmarkURL = $(".savedMarkURL");
 
-    for( var index = 0; index < 2; index++ ) {
-      // console.log( bookmarks[index]);
-      // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
-      bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
-      bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
-      bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
-    }
-
-   });
- */
-
-
-
-
-
-  $.ajax({
-    method: 'POST',
-    url: 'http://8cc094dc.ngrok.io/my_bookmarks',
-    data: { "username":"fake", "password":"password" },
-  })
-    .success(function (data) {
-    console.log("success");
-   $.ajax({
-     dataType: "JSON",
-     url: "https://tiyeventapi.herokuapp.com/dummy"
-   }) //end ajax GET request
-    .success(function (data) {
-      console.log("success");
-
-     var bookmarkName = $(".savedBookmarkItem");
-     var bookmarkDescrip = $(".savedMarkDescrip");
-     var bookmarkURL = $(".savedMarkURL");
-
-     for( var index = 0; index < 2; index++ ) {
-       // console.log( bookmarks[index]);
-       // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
-       bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
-       bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
-       bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
-     } // end for loop
-
+    for( var index = 0; index < data.length; index++ ) {
+    //  console.log(index);
+     // console.log( bookmarks[index]);
+     // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
+     bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
+     bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
+     bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
+    } // end for loop
    }); // end GET success
   }); // end POST success
-
 
   $( "#NewBookMarkDescription" ).keypress( function(e) {
     if( e.which === 13 ) {
@@ -69,29 +40,17 @@ $( function () {
 
       $.ajax({
         method: "POST",
-        url: "http://8cc094dc.ngrok.io/my_bookmarks",
+        url: "https://dummyplock.herokuapp.com/my_bookmarks",
         data: { "username":"fake", "password":"password", "bookmark_name": $('input[name="saveaBookmark"]').val(), "bookmark_description": $('input[name="saveaDescrip"]').val(),
                 "bookmark_url": $('input[name="saveaURL"]').val()
               }
-      }) // end ajax POST
-      // .success(function() {
-      //
-      // })
-      //
-      // .success(function (data) {
-      //   console.log("success");
+        }) // end ajax POST
+      .success(function() {
+        $( "#NewBookMarkName" ).val("");
+        $( "#NewBookMarkURL" ).val("");
+        $( "#NewBookMarkDescription" ).val("");
+      })
     } // end if
   }); // end keypress()
-
-
-
-
-
-
-
-
-
-
-
 
 }); // end outer function
