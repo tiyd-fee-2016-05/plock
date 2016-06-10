91cb7dd0ea1680c2fbff897896ab9979fa43d9c2
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-10 13:45:46 -0400

    Add functionality so that user can save bookmark to database...no output yet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,19 +37,16 @@ $( function () {
     method: 'POST',
     url: 'http://8cc094dc.ngrok.io/my_bookmarks',
     data: { "username":"fake", "password":"password" },
-  }).success(function (data) {
+  })
+    .success(function (data) {
     console.log("success");
    $.ajax({
      dataType: "JSON",
      url: "https://tiyeventapi.herokuapp.com/dummy"
-    })
+   }) //end ajax GET request
     .success(function (data) {
       console.log("success");
-     //  $.each( data.toArray(), function(index, value) {
-     //    console.log(value[0]);
-     //  });
-     // Array.from(data);
-     // console.log(Array.from(data)[0].bookmark_name);
+
      var bookmarkName = $(".savedBookmarkItem");
      var bookmarkDescrip = $(".savedMarkDescrip");
      var bookmarkURL = $(".savedMarkURL");
@@ -60,14 +57,31 @@ $( function () {
        bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
        bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
        bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
-     }
+     } // end for loop
 
-    });
-
-  });
-
+   }); // end GET success
+  }); // end POST success
 
 
+  $( "#NewBookMarkDescription" ).keypress( function(e) {
+    if( e.which === 13 ) {
+      e.preventDefault();
+
+      $.ajax({
+        method: "POST",
+        url: "http://8cc094dc.ngrok.io/my_bookmarks",
+        data: { "username":"fake", "password":"password", "bookmark_name": $('input[name="saveaBookmark"]').val(), "bookmark_description": $('input[name="saveaDescrip"]').val(),
+                "bookmark_url": $('input[name="saveaURL"]').val()
+              }
+      }) // end ajax POST
+      // .success(function() {
+      //
+      // })
+      //
+      // .success(function (data) {
+      //   console.log("success");
+    } // end if
+  }); // end keypress()
 
 
 
@@ -78,4 +92,6 @@ $( function () {
 
 
 
-});
+
+
+}); // end outer function
