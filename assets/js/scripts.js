733315fe7d7c17aa6c8ea987bde9ae415f540944
SINOVIA <somayfield@gmail.com>
2016-06-12 15:14:36 -0400

    Reloads bookmarks on submit, Bookmark layout works

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,8 @@ $( function () {
 
     var plockUser = $('.userInfo[name="user"]').val();
      var plockPassword = $('.passwordInfo[name="password"]').val();
-     var JSONurl = 'http://8cc094dc.ngrok.io/my_bookmarks'
+     var JSONurl = 'https://dummyplock.herokuapp.com/my_bookmarks';
+
     console.log(plockUser);
     console.log(plockPassword);
 
@@ -45,7 +46,7 @@ $( function () {
 
       var plockUser = $('.userInfo[name="user"]').val();
        var plockPassword = $('.passwordInfo[name="password"]').val();
-       var JSONurl = 'http://8cc094dc.ngrok.io/my_bookmarks'
+       var JSONurl = 'https://dummyplock.herokuapp.com/my_bookmarks'
 
       $.ajax({
         method: "POST",
@@ -53,15 +54,35 @@ $( function () {
         data: { "username": plockUser, "password":plockPassword, "bookmark_name": $('input[name="saveaBookmark"]').val(), "bookmark_description": $('input[name="saveaDescrip"]').val(),
                 "bookmark_url": $('input[name="saveaURL"]').val()
               }
-      }) // end ajax POST
+      })
+      $.ajax({
+        method: 'GET',
+        url: JSONurl,
+        data: { "username": plockUser, "password": plockPassword},
+      })
+        .success(function (data) {
+          console.log("success");
+
+         var bookmarkName = $(".savedBookmarkItem");
+         var bookmarkDescrip = $(".savedMarkDescrip");
+         var bookmarkURL = $(".savedMarkURL");
+
+         for( var index = 0; index < 7; index++ ) {
+           console.log(index);
+           // console.log( bookmarks[index]);
+           // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
+           bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
+           bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
+           bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
+         } // end for loop
+         alert("Bookmark Submitted!")
+       });// end ajax POST
       // .success(function() {
       //
       // })
       //
       // .success(function (data) {
-      //   console.log("success");
-    } // end if
-  }); // end keypress()
+      //   console.log("success"); end keypress()
 
   $(document).ready(function() {
 
@@ -85,7 +106,7 @@ $( function () {
 
         var plockUser = $('.userInfo[name="user"]').val();
          var plockPassword = $('.passwordInfo[name="password"]').val();
-         var JSONrecurl = 'http://8cc094dc.ngrok.io/recommendations'
+         var JSONrecurl = 'https://dummyplock.herokuapp.com/my_bookmarks'
 
         console.log( Array.from(bookmarksReceived)[0].id );
         $.ajax({
@@ -104,3 +125,32 @@ $( function () {
           // console.log( "Recommendation Made" );
         })
     }); // end keypress()
+
+    // POST to recommend a bookmark
+      $( ".Bookmark1" ).click( function(e) {
+          e.preventDefault();
+
+          var plockUser = $('.userInfo[name="user"]').val();
+           var plockPassword = $('.passwordInfo[name="password"]').val();
+           var JSONrecURL = 'https://dummyplock.herokuapp.com/my_bookmarks'
+
+          console.log( Array.from(bookmarksReceived)[0].id );
+          $.ajax({
+            method: "POST",
+            url: "http://8cc094dc.ngrok.io/recommendations",
+            data: { "username":"fake", "password":"password", "bookmark_id": Array.from(bookmarksReceived)[0].id,
+                    "recipient":"recipient"
+                  }
+
+
+            }) // end ajax POST
+          .success(function() {
+            // $( "#NewBookMarkName" ).val("");
+            // $( "#NewBookMarkURL" ).val("");
+            // $( "#NewBookMarkDescription" ).val("");
+            // console.log( "Recommendation Made" );
+          })
+      }); // end keypress()
+
+    };
+  });
