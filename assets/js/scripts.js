c918827ed532e7413cdc648795f2f905353589b6
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-12 16:16:45 -0400

    Added functionality so when you click suggest a bookmark, it grabs the id of that bookmark and then sends it, along with other params to the server...which is down right now

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,7 +14,9 @@ $( function () {
   // GET will submit username and password and then retrieve their bookmarks
   $.ajax({
     method: 'GET',
-    url: 'http://8cc094dc.ngrok.io/my_bookmarks',
+
+    url: 'https://dummyplock.herokuapp.com/my_bookmarks',
+    // url: 'http://8cc094dc.ngrok.io/my_bookmarks',
     data: { "username": plockUser, "password": plockPassword},
   })
     .success(function (data) {
@@ -30,6 +32,7 @@ $( function () {
     //  console.log(index);
      // console.log( bookmarks[index]);
      // This is OK for now, but will need to switch to creating the <li> here in js if we want this dynamic
+     // line 33 is throwing: scripts.js:33 Uncaught TypeError: Cannot set property 'innerHTML' of undefined
      bookmarkName[index].innerHTML = ( Array.from(data)[index].bookmark_name );
      bookmarkDescrip[index].innerHTML = ( Array.from(data)[index].bookmark_description );
      bookmarkURL[index].innerHTML = ( Array.from(data)[index].bookmark_url );
@@ -44,7 +47,8 @@ $( function () {
 
       $.ajax({
         method: "POST",
-        url: "http://8cc094dc.ngrok.io/my_bookmarks",
+        url: "https://dummyplock.herokuapp.com/my_bookmarks",
+        // url: 'http://8cc094dc.ngrok.io/my_bookmarks',
         data: { "username":"fake", "password":"password", "bookmark_name": $('input[name="saveaBookmark"]').val(), "bookmark_description": $('input[name="saveaDescrip"]').val(),
                 "bookmark_url": $('input[name="saveaURL"]').val()
               }
@@ -55,20 +59,71 @@ $( function () {
         $( "#NewBookMarkDescription" ).val("");
       })
     } // end if
-  }); // end keypress()
+  }); // end POST event keypress() to save a bookmark
 
-  // POST to recommend a bookmark
-  $( ".Bookmark1" ).click( function(e) {
-      e.preventDefault();
-      console.log( Array.from(bookmarksReceived)[0].id );
+
+  // click event to make a recommendation
+
+  var recommendBtn = $( ".recommendBtn" );
+
+  // var bookmarkList = Array.from( $( ".individual-bookmark" ) );
+  $(".savedBookmarkList").on( "click", ".recommendBtn", function(e) {
+    e.preventDefault();
+    // e.stopPropagation();
+
+    for( var index = 0; index < recommendBtn.length; index++ ) {
+      if( this === recommendBtn[index] ) {
+        console.log( "Number: " + index );
+        console.log( "ID: " + Array.from(bookmarksReceived)[index].id );
+
+        $.ajax({
+          method: "POST",
+          url: "https://dummyplock.herokuapp.com/recommendations",
+          // url: 'http://8cc094dc.ngrok.io/recommendations',
+          data: { "username":"fake", "password":"password", "bookmark_id": Array.from(bookmarksReceived)[index].id,
+                  "recipient":"recipient"
+                }
+          }) // end ajax POST
+        .success(function() {
+          // $( "#NewBookMarkName" ).val("");
+          // $( "#NewBookMarkURL" ).val("");
+          // $( "#NewBookMarkDescription" ).val("");
+          // console.log( "Recommendation Made" );
+        })
+      }
+    }
+
+    // console.log( $(bookmarkList) );
+    // console.log( this );
+    // console.log( bookmarkList.indexOf(this) );
+    // console.log( $.inArray( this, bookmarkList) );
+
+
+
+    // fruits.indexOf("Apple");
+
+
+
+    // console.log( $(this)[0] );
+    // console.log( $(this) );
+    // console.log( $(this)[0].closest("h3") );
+
+    // var $elementlist = $(this).parent;
+    // console.log('Element ' + $(elementlist).index($(this)) + ' was clicked');
+
+
+      // console.log( e.target );
+      // console.log( Array.from($(this).bookmarksReceived) );
+      // console.log( $(this).index(bookmarksReceived).val());
+      // console.log( $(this).closest(".individual-bookmark")[e]);
+      // console.log( $(this).closest(bookmarksReceived));
+      /*
       $.ajax({
         method: "POST",
-        url: "http://8cc094dc.ngrok.io/recommendations",
+        url: "https://dummyplock.herokuapp.com/my_bookmarks",
         data: { "username":"fake", "password":"password", "bookmark_id": Array.from(bookmarksReceived)[0].id,
                 "recipient":"recipient"
               }
-
-
         }) // end ajax POST
       .success(function() {
         // $( "#NewBookMarkName" ).val("");
@@ -76,6 +131,41 @@ $( function () {
         // $( "#NewBookMarkDescription" ).val("");
         // console.log( "Recommendation Made" );
       })
-  }); // end keypress()
+      */
+  }); // end recommendation click event()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // function to show bookmarks and recommendations
+  $(document).ready(function() {
+
+  // show recommendations
+  $('.showRecs').on('click', function() {
+      $('.recTab').css('display', 'block');
+      $('.bookmarkTab').css('display', 'none');
+      $('.startingTab').css('display', 'none');
+
+      console.log("Click works again!");
+  });
+
+  // show bookmarks
+  $('.showBooksmarks').on('click', function() {
+      $('.bookmarkTab').css('display', 'block');
+      $('.recTab').css('display', 'none');
+      $('.startingTab').css('display', 'none');
+      console.log("Second Click works");
+    });
+  }); // end function to show bookmarks and recommendations
 }); // end outer function
